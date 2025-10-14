a882b9db10221845e7c6514d19071dd74bc09a74
777aker <777aker@gmail.com>
2025-10-13 18:23:23 -0600

    init project with lve

---
+++ compile_shaders.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+/home/kelley/vulkan/1.4.321.1/x86_64/bin/glslc /home/kelley/Desktop/vulkan_tutorial/shaders/simple_shader.vert -o /home/kelley/Desktop/vulkan_tutorial/shaders/simple_shader.vert.spv
+/home/kelley/vulkan/1.4.321.1/x86_64/bin/glslc /home/kelley/Desktop/vulkan_tutorial/shaders/simple_shader.frag -o /home/kelley/Desktop/vulkan_tutorial/shaders/simple_shader.frag.spv
---
+++ first_app.cpp
@@ -0,0 +1,62 @@
+#include "first_app.hpp"
+
+#include "simple_render_system.hpp"
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/glm.hpp>
+#include <glm/gtc/constants.hpp>
+
+#include <stdexcept>
+#include <array>
+#include <cassert>
+
+namespace lve
+{
+
+  FirstApp::FirstApp()
+  {
+    loadGameObjects();
+  }
+
+  FirstApp::~FirstApp() {}
+
+  void FirstApp::run()
+  {
+    SimpleRenderSystem simpleRenderSystem{lveDevice, lveRenderer.getSwapChainRenderPass()};
+
+    while (!lveWindow.shouldClose())
+    {
+      glfwPollEvents();
+
+      if (auto commandBuffer = lveRenderer.beginFrame())
+      {
+        lveRenderer.beginSwapChainRenderPass(commandBuffer);
+        simpleRenderSystem.renderGameObjects(commandBuffer, gameObjects);
+        lveRenderer.endSwapChainRenderPass(commandBuffer);
+        lveRenderer.endFrame();
+      }
+    }
+
+    vkDeviceWaitIdle(lveDevice.device());
+  }
+
+  void FirstApp::loadGameObjects()
+  {
+    std::vector<LveModel::Vertex> vertices{
+        {{0.0f, -0.5f}, {0.0f, 0.0f, 0.0f}},
+        {{0.5f, 0.5f}, {0.0f, 0.0f, 0.0f}},
+        {{-0.5f, 0.5f}, {0.0f, 0.0f, 0.0f}}};
+    auto lveModel = std::make_shared<LveModel>(lveDevice, vertices);
+
+    auto triangle = LveGameObject::createGameObject();
+    triangle.model = lveModel;
+    triangle.color = {0.1f, 0.8f, 0.1f};
+    triangle.transform2d.translation.x = 0.2f;
+    triangle.transform2d.scale = {0.2f, 2.0f};
+    triangle.transform2d.rotation = 0.25f * glm::two_pi<float>();
+
+    gameObjects.push_back(std::move(triangle));
+  }
+
+}
