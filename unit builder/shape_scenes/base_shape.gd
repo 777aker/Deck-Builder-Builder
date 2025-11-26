894ac2d018d616e0a6f43067bd19bfa4f1fc1e3c
Kelley <777aker@gmail.com>
2025-11-26 13:12:19 -0700

    basic shapes done

--- unit builder/shape_scenes/base_shape.gd
+++ unit builder/shape_scenes/base_shape.gd
@@ -22,6 +22,7 @@ func _process(_delta: float) -> void:
 
 func _on_area_2d_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
 	if event.is_action_pressed("left_click"):
+		get_parent().move_child(self, -1)
 		dragging = true
 	
 
